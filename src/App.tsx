3bcd8e560f03abf3b9d56736364f431bb16ad457
Sean Phaphui <seanphaphui@gmail.com>
2023-09-02 21:41:37 -0700

    add initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "./App.css";
+import "./font.css";
+import Home from "./Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div className="App">
+                <header className="App-header">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                    </Routes>
+                </header>
+            </div>
+        </LocalizationProvider>
+    );
 }
 
 export default App;
---
+++ src/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import { Button } from "@mui/material";
+
+
+const Home: React.FC = () => {
+    return (
+        <div className="Home">
+            
+        </div>
+    );
+};
+
+export default Home;
