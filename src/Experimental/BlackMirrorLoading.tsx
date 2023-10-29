c45d1de4e42d4e3ab7b160ae3ba123bc13418630
Sean Phaphui <seanphaphui@gmail.com>
2023-10-28 21:17:08 -0700

    add experimental loading component

---
+++ src/Experimental/BlackMirrorLoading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './BlackMirrorLoading.css';
+
+const BlackMirrorLoading: React.FC = () => {
+    return (
+        <div className="blackmirrorLoading">
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+            <div className="blackmirrorLoading-part"></div>
+        </div>
+    );
+}
+
+export default BlackMirrorLoading;
