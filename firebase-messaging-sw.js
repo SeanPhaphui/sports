3ad39f80e9118da387a74e00b817228736302c37
Sean Phaphui <seanphaphui@gmail.com>
2023-11-07 22:22:35 -0800

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // public/firebase-messaging-sw.js
 
 // These importScripts paths are for the modular v9+ SDK
-importScripts("https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 
 // Your Firebase config object
 const firebaseConfig = {
